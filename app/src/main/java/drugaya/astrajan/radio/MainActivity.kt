6e22a90a41d237d84a6be436182d00b976b17b09
alexei <alexei.saron@gmail.com>
2021-10-16 23:43:39 +0200

    one1

--- app/src/main/java/drugaya/astrajan/radio/MainActivity.kt
+++ app/src/main/java/drugaya/astrajan/radio/MainActivity.kt
@@ -3,14 +3,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.core.content.ContextCompat
 import drugaya.astrajan.radio.rossiya_app.util.App.Companion.player
 import drugaya.astrajan.radio.rossiya_app.util.App.Companion.stopRadio
 import drugaya.astrajan.radio.rossiya_app.util.ServiceRadio
 import kotlinx.android.synthetic.main.activity_main.*
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() { // // video_view.player = player
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,9 +27,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        if(player != null){ if(player!!.isPlaying){ start.visibility = View.GONE; stop.visibility = View.VISIBLE }
-            if(player!!.isPlaying){ start.visibility = View.GONE; stop.visibility = View.VISIBLE }
-            if(player!!.isPlaying){ start.visibility = View.GONE; stop.visibility = View.VISIBLE }
-        }
+        if(player != null){ if( player!!.isPlaying ){ start.visibility = View.GONE; stop.visibility = View.VISIBLE; } }
     }
 }
--- app/src/main/java/drugaya/astrajan/radio/rossiya_app/util/App.kt
+++ app/src/main/java/drugaya/astrajan/radio/rossiya_app/util/App.kt
@@ -5,32 +5,32 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import androidx.core.app.NotificationCompat
-import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.source.ProgressiveMediaSource
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import drugaya.astrajan.radio.MainActivity
 import drugaya.astrajan.radio.R
 
 class App: Application() {
     companion object{
         lateinit var notification: Notification
-        var player : SimpleExoPlayer? = null
+        var player: SimpleExoPlayer? = null
 
         fun playExoplayer(context: Context){
-            val content : ProgressiveMediaSource = ProgressiveMediaSource.Factory(DefaultDataSourceFactory(context , "Mozilla Firefox"))
-                .createMediaSource(Uri.parse( "http://89.179.72.53:8070/live" ))
-            player!!.prepare(content)
-            player!!.playWhenReady = true
+            player!!.setMediaItem(MediaItem.fromUri("http://89.179.72.53:8070/live"))
+            player!!.prepare()
+            player!!.play()
         }
 
-        fun stopRadio(){ if(player!!.isPlaying){ player!!.stop() } }
+        fun stopRadio(){
+            if( player != null ){
+                if(player!!.isPlaying){ player!!.stop() }
+            }
+        }
     }
 
     override fun onCreate(){
         super.onCreate()
-
-        player = ExoPlayerFactory.newSimpleInstance(this)
+        player = SimpleExoPlayer.Builder(this ).build()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val serChannel = NotificationChannel("drugaya-astrajan-radio", "name", NotificationManager.IMPORTANCE_LOW)
